ae8106ab75843392aa06279fcf1ae9d838feb04e
a914622719 <914622719@qq.com>
2024-09-28 22:02:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import base64
 import ddddocr
 import feapder
 import argparse
+import requests
 
 from env import *
 from feapder.utils.log import log
+
+global bark_log
 
 
 class CQ(feapder.AirSpider):
@@ -21,16 +24,20 @@ class CQ(feapder.AirSpider):
 
     def start_requests(self):
         log.info("开始执行")
+        global bark_log
+        bark_log = bark_log + f"用户名：{USERNAME}\n"
         log.info(f"用户名：{USERNAME}")
         self.send_msg("开始执行", level="INFO")
         code_url = "https://ids.gzist.edu.cn/lyuapServer/kaptcha"
         yield feapder.Request(url=code_url, callback=self.parse_tryLogin)
 
     def parse_tryLogin(self, request, response):
+        global bark_log
         login_url = "https://ids.gzist.edu.cn/lyuapServer/v1/tickets"
         uid = response.json["uid"]
         code_base64_str = response.json["content"].split(",")[-1]
         code, code_result = self.code_ocr(code_base64_str)
+        bark_log = bark_log + f"验证码: {code};答案: {code_result}\n"
         log.info(f"验证码: {code};答案: {code_result}")
         post_data = {
             "username": USERNAME,
@@ -45,18 +52,24 @@ class CQ(feapder.AirSpider):
         except KeyError:
             data_code = login_response["data"]["code"]
             if data_code == 'NOUSER':
+                bark_log = bark_log + "用户名错误\n"
                 raise self.InfoError(fr"用户名错误")
             elif data_code == 'PASSERROR':
+                bark_log = bark_log + "密码错误\n"
                 raise self.InfoError(fr"密码错误")
             elif data_code == 'CODEFALSE':
+                bark_log = bark_log + "fr验证码错误,尝试重新运行,{request.retry_times}\n"
                 raise self.CodeError(fr"验证码错误,尝试重新运行,{request.retry_times}", code=code,
                                      code_result=code_result)
             elif data_code == 'ISMODIFYPASS':
+                bark_log = bark_log + "密码未修改\n"
                 raise self.InfoError(fr"密码未修改")
             elif data_code == 'ISPHONEOREMAILORANSWER':
+                bark_log = bark_log + "未绑定手机或邮箱或密保问题\n"
                 raise self.InfoError(fr"未绑定手机或邮箱或密保问题")
             raise KeyError(fr"返回值未知,尝试重新运行: {data_code}")
         except Exception as e:
+            bark_log = bark_log + fr"发生未知错误,尝试重新运行: {e}\n"
             raise Exception(fr"发生未知错误,尝试重新运行: {e}")
         jump_url = "https://xsfw.gzist.edu.cn/xsfw/sys/swmzncqapp/*default/index.do"
         yield feapder.Request(
@@ -86,29 +99,38 @@ class CQ(feapder.AirSpider):
             cookies=cookies)
 
     def parse(self, request, response):
+        global bark_log
         try:
             result = response.json["msg"]
             if result == ' 当前时段不在考勤时段内':
+                bark_log = bark_log + f"::warning:: {result}\n"
                 log.warning(f"::warning:: {result}")
                 self.send_msg(result, "INFO")
                 return
             elif result == ' 您已签到,请勿重复签到':
+                bark_log = bark_log + "您已签到,请勿重复签到'\n"
                 pass
             log.info(fr"查寝结果：{result}")
+            bark_log = bark_log + fr"查寝结果：{result}\n"
             self.send_msg(result, "INFO")
         except Exception as e:
+            bark_log = bark_log + f"::error:: 查寝失败，结果未知：{e}\n"
             log.error(f"::error:: 查寝失败，结果未知：{e}")
 
     def exception_request(self, request, response, e: Exception):
+        global bark_log
         if type(e) is self.InfoError:
             self.send_msg(f"{e}", "ERROR")
             self.stop_spider()
             tools.delay_time(1)
         elif type(e) is self.CodeError:
+            bark_log = bark_log + f"验证码错误\n验证码:{e.code}\n答案:{e.code_result}\n"
             self.send_msg(f"验证码错误\n验证码:{e.code}\n答案:{e.code_result}", "ERROR")
         elif type(e) is KeyError:
+            bark_log = bark_log + f"返回值未知：{e}\n"
             self.send_msg(f"返回值未知：{e}", "ERROR")
         elif type(e) is Exception:
+            bark_log = bark_log + f"发生未知错误：{e}\n"
             self.send_msg(f"发生未知错误：{e}", "ERROR")
 
         log.error(f"::error:: {e}")
@@ -160,6 +182,9 @@ class CQ(feapder.AirSpider):
 def get_username_password_from_env():
     username = os.environ.get("LOGIN_USERNAME")
     password = os.environ.get("LOGIN_PASSWORD")
+    global bark_log
+    bark_log = os.environ.get("BARK_URL")
+
     return username, password
 
 
@@ -204,9 +229,14 @@ if __name__ == '__main__':
     USERNAME, PASSWORD = get_username_password()
     if USERNAME and PASSWORD:
         CQ().start()
+
     else:
         if not USERNAME:
+            bark_log = bark_log + "::error:: 账号不能为空\n"
             log.error("::error:: 账号不能为空")
         if not PASSWORD:
+            bark_log = bark_log + "::error:: 密码不能为空\n"
             log.error("::error:: 密码不能为空")
-        sys.exit(1)
+
+    requests.get(bark_log)
+    sys.exit(1)
