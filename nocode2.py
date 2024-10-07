edbdb511a982a944710b1fccf05f60bf6ae1cf96
Today <914622719@qq.com>
2024-10-07 23:42:16 +0800

    Update nocode2.py

--- nocode2.py
+++ nocode2.py
@@ -137,8 +137,8 @@ class CQ(feapder.AirSpider):
 
 
 def get_username_password_from_env():
-    username = os.environ.get("LOGIN_USERNAME")
-    password = os.environ.get("LOGIN_PASSWORD")
+    username = os.environ.get("LOGIN_USERNAME2")
+    password = os.environ.get("LOGIN_PASSWORD2")
     global bark_log
     bark_log = os.environ.get("BARK_URL")
 
@@ -149,8 +149,8 @@ def get_username_password_from_config(config_file, section):
     config = configparser.ConfigParser()
     config.read(config_file)
     if config.has_section(section):
-        username = config.get(section, 'LOGIN_USERNAME')
-        password = config.get(section, 'LOGIN_PASSWORD')
+        username = config.get(section, 'LOGIN_USERNAME2')
+        password = config.get(section, 'LOGIN_PASSWORD2')
         return username, password
     else:
         return None, None
